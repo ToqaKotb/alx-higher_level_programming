81d11e62df5ac1e497cdc72af9168073339bcac7
ToqaKotb <htoqa13@yahoo.com>
2024-01-10 10:10:39 -0800

    gg

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return(set_1.symmetric_difference(set_2))
+    """Return a set of all elements present in only one set."""
+    return (set_1 ^ set_2)
