8c34dd66f1ca4b758af22339453560d4dba80f8b
ToqaKotb <htoqa13@yahoo.com>
2024-01-10 09:08:02 -0800

    jj

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,9 @@
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
-
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-
         for i in range(4, 6):
             c = add(c, i)
+        return(c)
 
-        return c
-
-    return sub(a, b)
+    return(sub(a, b))
