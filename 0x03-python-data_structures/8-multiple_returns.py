b3ca7e61aa333cd1e5f0559c11c0dd09e4d0c68d
ToqaKotb <htoqa13@yahoo.com>
2024-01-09 18:59:42 -0800

    hh

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    total = len(sentence)
-    char = sentence[0] if total > 0 else "None"
-    new = total, char
-    return(new)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
