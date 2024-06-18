028ed80174a36e1429540e3ca47b3f5ac5ff707a
ToqaKotb <143521220+ToqaKotb@users.noreply.github.com>
2024-06-19 01:37:15 +0300

    Update 3-value_argument.jsmm

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (arg === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(arg);
+  console.log(process.argv[2]);
 }
