496e6ef0ca577c8c282c99eb038d1499d7ddab41
ToqaKotb <143521220+ToqaKotb@users.noreply.github.com>
2024-06-19 01:31:42 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-let nextMax = 0;
-
-if (args.length > 1) {
-  const integers = args.map(Number);
-  integers.sort((a, b) => b - a);
-  nextMax = integers[1];
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
-
-console.log(nextMax);
