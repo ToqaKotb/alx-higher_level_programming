370aef131e26d769f1ec5d04d0076755f06449fd
ToqaKotb <143521220+ToqaKotb@users.noreply.github.com>
2024-06-19 01:19:33 +0300

    3

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-const a = process.argv[2];
-function factorial (a) {
-  if (isNaN(a) || a === 1) {
-    return (1);
-  } else {
-    return (a * factorial(a - 1));
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
   }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
 }
-console.log(factorial(parseInt(a)));
+
+console.log(factorial(Number(process.argv[2])));
