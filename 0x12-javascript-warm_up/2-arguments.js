80625de3dd194480a1cb97f513f657c87d2e85fe
ToqaKotb <143521220+ToqaKotb@users.noreply.github.com>
2024-06-19 01:36:07 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
-const count = process.argv.length;
-
-if (count === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (count === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
+}
